159d2ecdd99150bd9452191783facd0f5b58e747
asp_789 <asp_789@DESKTOP-0GQ8JN7>
2018-10-19 17:04:13 -0500

    informe de ventas por empresa solagro

---
+++ JSP7_Cotizaciones_Rep/src/main/java/com/aspsols/cotizaciones/reportes/informeDeVentasPorEmpresaParamForm.java
@@ -0,0 +1,29 @@
+package com.aspsols.cotizaciones.reportes;
+
+import com.aspsolutions.jframework.Server.IJProduct;
+import com.aspsolutions.jframework.Server.startup.JFormApplication;
+import com.aspsolutions.jreports.server.parameters.RuntimeParameterFormMVC;
+
+@JFormApplication(controller = "informeDeVentasPorEmpresaParamForm.controller", model = "informeDeVentasPorEmpresaParamForm.model", version = "1.0", view = "informeDeVentasPorEmpresaParamForm.view", jspDocument = "reports/com/aspsols/cotizaciones/reportes/informeDeVentasPorEmpresaParamForm.jsp")
+public class informeDeVentasPorEmpresaParamForm extends RuntimeParameterFormMVC implements IJProduct{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Codigo generado por el usuario
+	 */
+
+
+
+
+	
+	public informeDeVentasPorEmpresaParamForm () throws Exception {
+	    	}
+	
+		
+
+
+}
---
+++ JSP7_Cotizaciones_Rep/src/main/java/com/aspsols/cotizaciones/reportes/informeDeVentasPorEmpresa_class2.java
@@ -0,0 +1,115 @@
+package com.aspsols.cotizaciones.reportes;
+
+import java.awt.Font;
+import  java.util.Map;
+import java.util.Vector;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.util.Iterator;
+import java.util.Hashtable;
+import com.aspsolutions.jdbc.ObjectType;
+import com.aspsolutions.jframework.Exceptions.RecordIdNotFoundException;
+import com.aspsolutions.jframework.standar.JReportUtils;
+import com.aspsolutions.jreports.server.JReport;
+import com.aspsolutions.jreports.server.PresentationRules;
+import com.aspsolutions.jreports.server.QueryLogic;
+import com.aspsolutions.jreports.server.presentation.FormatException;
+import com.aspsolutions.jreports.server.presentation.ReportEllipse;
+import com.aspsolutions.jreports.server.presentation.ReportFieldPresentation;
+import com.aspsolutions.jreports.server.presentation.ReportLine;
+import com.aspsolutions.jreports.server.presentation.ReportMargin;
+import com.aspsolutions.jreports.server.presentation.ReportRectangle;
+import com.aspsolutions.jreports.server.presentation.ReportImage;
+import com.aspsolutions.jreports.server.presentation.ReportRegion;
+import com.aspsolutions.jreports.server.presentation.ReportRepetitiveFrame;
+import com.aspsolutions.jreports.server.presentation.ReportSection;
+import com.aspsolutions.jreports.server.querylogic.DataLink;
+import com.aspsolutions.jreports.server.querylogic.Group;
+import com.aspsolutions.jreports.server.querylogic.GroupField;
+import com.aspsolutions.jreports.server.querylogic.Query;
+import com.aspsolutions.jreports.server.querylogic.querycomponents.FromTables;
+import com.aspsolutions.jreports.server.runtime.GroupPointer;
+import com.aspsolutions.jreports.server.runtime.JReportRuntime;
+import com.aspsolutions.jreports.server.runtime.QueryRecords;
+import com.aspsolutions.jreports.server.runtime.ReportField;
+import com.aspsolutions.jreports.server.presentation.ReportText;
+import com.aspsolutions.jreports.server.presentation.ReportFrame;
+import com.aspsolutions.jreports.server.presentation.ReportRoundedRectangle;
+import com.aspsolutions.jframework.standar.ASPString;
+import com.aspsolutions.jreports.server.querylogic.querycomponents.SelectColumns;
+import com.aspsolutions.jreports.server.parameters.ParameterForm;
+import com.aspsolutions.jframework.Server.DataFieldDefinition;
+import com.aspsolutions.jframework.Server.GraphicsObjectServer;
+import javax.swing.FocusManager;
+import com.aspsolutions.jframework.standar.ASPMath;
+import com.aspsolutions.jframework.Server.RecordGroupServer;
+import com.aspsolutions.jframework.Server.HelpListServer;
+import com.aspsolutions.jreports.server.presentation.ReportChart;
+import com.aspsolutions.jframework.Server.JApplicationServer;
+import com.aspsolutions.jframework.standar.ASPConverter;
+import com.aspsolutions.jframework.standar.ASPSQLFunctions;
+import com.aspsolutions.jframework.Utils.SqlParameter;
+import com.aspsolutions.jframework.Exceptions.NoDataFoundException;
+import com.aspsolutions.jframework.Exceptions.TooManyRows;
+import com.aspsolutions.jframework.Exceptions.NumericOrValueException;
+import com.aspsolutions.jframework.srw.SRWProgramAbort;
+import com.aspsolutions.jframework.standar.ASPLogicalOperators;
+import java.awt.Font;
+import com.aspsolutions.jreports.server.LayoutTextVariables;
+import com.aspsolutions.jframework.Server.ProcedureCallParameters;
+import com.aspsolutions.jframework.standar.Utils;
+import com.aspsolutions.jreports.server.presentation.PresentationFormat;
+import com.aspsolutions.jreports.server.presentation.Conditions;
+import com.aspsolutions.jframework.Server.ColumnSpecifications;
+import com.aspsolutions.jreports.server.presentation.ReportButton;
+import com.aspsolutions.jreports.server.presentation.PageNumbering;
+import com.aspsolutions.jframework.Exceptions.DupValOnIndexException;
+import com.aspsolutions.jreports.server.presentation.ReportAnchor;
+import com.aspsolutions.jframework.standar.ASPCollection;
+import com.aspsolutions.jreports.server.presentation.ChartParameter;
+import com.aspsolutions.jreports.server.presentation.ChartColumn;
+import static com.aspsolutions.jframework.standar.ASPConverter.*;
+import static com.aspsolutions.jframework.standar.ASPLogicalOperators.*;
+import static com.aspsolutions.jframework.standar.ASPMath.*;
+import static com.aspsolutions.jframework.standar.ASPString.*;
+import com.aspsolutions.jframework.standar.ASPAccessFile;
+import com.aspsolutions.jframework.srw.SRWRunReportFailure;
+
+public class informeDeVentasPorEmpresa_class2 extends informeDeVentasPorEmpresa_class1 {
+	protected ReportFrame reportFrameMainBodyFrame0 = new ReportFrame("FRAME0",155,6,21,209,reportRegionMainBody,0,null,"VARIABLE","VARIABLE",new Color(255,255,255),null,"DEFAULT",false,false,false,null,0,-1,false);
+	protected ReportRepetitiveFrame repetitiveFrameMainBodyRepeating_frame0 = new ReportRepetitiveFrame("REPEATING_FRAME0",227,6,20,61,reportRegionMainBody,1,reportFrameMainBodyFrame0,"FIXED","EXPAND","GROUP0","Across",new Color(255,255,255),null,0,"DEFAULT",false,false,false,null,0,0,0,-1,false);
+	protected DataFieldDefinition dataFieldDefinitionParameterperiodo = new DataFieldDefinition("parameterperiodo",143,80,120,20,new Color(255,255,255),new Color(0,0,0),null,null,"TF","java.lang.String",true,new Font("Arial",0,10),null,null,null,false,true,null,null,false,null,"",null,2,true,20,null,null,false,"periodo");
+	protected GraphicsObjectServer graphicServergraphicTitulo = new GraphicsObjectServer("Titulo",new Color(255,255,255),new Color(0,0,0),"TEXT_GRAPHIC",20,120,100,40,false,false,-1,0,null,0,0,new Font("Arial",1,14),false,"Report parameters");
+	protected GraphicsObjectServer graphicServergraphicParameterperiodo = new GraphicsObjectServer("parameterperiodo",new Color(255,255,255),new Color(0,0,0),"TEXT_GRAPHIC",20,120,80,80,false,false,-1,0,null,0,0,new Font("Arial",0,10),false,"periodo");
+
+	// System parameters
+	protected GroupField parameterBACKGROUND = new GroupField("BACKGROUND","java.lang.String","false",true,this.tempGroup,this);
+	protected GroupField parameterCOPIES = new GroupField("COPIES","java.lang.Double","1",true,this.tempGroup,this);
+	protected GroupField parameterCURRENCY = new GroupField("CURRENCY","java.lang.String",null,true,this.tempGroup,this);
+	protected GroupField parameterDECIMAL = new GroupField("DECIMAL","java.lang.String","0",true,this.tempGroup,this);
+	protected GroupField parameterDESFORMAT = new GroupField("DESFORMAT","java.lang.String","dec",true,this.tempGroup,this);
+	protected GroupField parameterDESNAME = new GroupField("DESNAME","java.lang.String",null,true,this.tempGroup,this);
+	protected GroupField parameterDESTYPE = new GroupField("DESTYPE","java.lang.String","Preview",true,this.tempGroup,this);
+	protected GroupField parameterMODE = new GroupField("MODE","java.lang.String","Default",true,this.tempGroup,this);
+	protected GroupField parameterORIENTATION = new GroupField("ORIENTATION","java.lang.String","Default",true,this.tempGroup,this);
+	protected GroupField parameterPRINTJOB = new GroupField("PRINTJOB","java.lang.String","false",true,this.tempGroup,this);
+	protected GroupField parameterTHOUSANDS = new GroupField("THOUSANDS","java.lang.String","0",true,this.tempGroup,this);
+	protected GroupField parameterMAXRECORDS = new GroupField("MAXRECORDS","java.lang.Integer","0",true,this.tempGroup,this);
+
+	// User parameters
+	protected GroupField parameterPeriodo = new GroupField("periodo","java.lang.String",null,"",this.tempGroup,this);
+
+			
+	public informeDeVentasPorEmpresa_class2() throws Exception {
+		super();
+	}
+}
